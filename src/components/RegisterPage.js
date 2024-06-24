7dfaf6affe170d0e43ff1e37a6988d9d31b98214
rohinirai010 <rairohini2001@gmail.com>
2024-06-24 12:26:08 +0530

    third commit

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './RegisterPage.css'; // Import the CSS file
+import './RegisterPage.css'; 
+import { useNavigate } from 'react-router-dom';
 
 const RegisterPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleRegister = async (e) => {
         e.preventDefault();
@@ -17,6 +19,11 @@ const RegisterPage = () => {
         }
     };
 
+    const handleLogin = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
     return (
         <div className="register-container">
             <h2>Register</h2>
@@ -24,6 +31,7 @@ const RegisterPage = () => {
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="register-input" placeholder="Email" required />
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="register-input" placeholder="Password" required />
                 <button type="submit" className="register-button">Register</button>
+                <button type="submit" className="login-button" onClick={handleLogin}>Go to Login Page</button>
             </form>
             <p className="register-message">{message}</p>
         </div>
